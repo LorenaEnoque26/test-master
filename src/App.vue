25191d98812b4901fa560384fb620df36509beae
LorenaEnoque26 <lorenaenoque@hotmail.com>
2020-05-25 16:41:30 -0300

    event filter by date

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,43 @@
 <template>
-  <div class="app">
-    <section class="app__components">
-      <div class="app__components__events">
-        <!-- bootstrap data card-->
+  <div id="app">
+    <div class="app__components">
+      <!-- filters events by date -->
+      <section class="app__components__dates">
+        <b-input-group>
+          <!-- bootstrap calendar -->
+          <b-form-datepicker
+            close-button
+            locale="en"
+            placeholder="Choose a date"
+            id="datepicker"
+            :min="min"
+            v-model="filterDate"
+            class="app__components__dates__datepicker"
+          >
+          </b-form-datepicker>
+          <!-- bootstrap calendar - end -->
+
+          <!-- button reset date -->
+          <template v-slot:append v-if="filterDate !== null">
+            <b-button @click="filterDate = null">
+              <b-icon icon="x"></b-icon>
+            </b-button>
+          </template>
+          <!-- button reset date - end -->
+        </b-input-group>
+      </section>
+      <!-- filters events by date - end-->
+
+      <section class="app__components__events">
+        <!-- bootstrap data card -->
         <b-card
           no-body
           bg-variant="transparent"
-          v-for="event in events"
+          v-for="event in filteredData"
           :key="event.title"
           class="app__components__events__card "
         >
-          <!-- card images -->
+          <!-- render images (when you click changes it will go to the event page) -->
           <div class="app__components__events__card__img">
             <a :href="event.link" target="_blank">
               <img
@@ -43,23 +70,26 @@
               />
             </a>
           </div>
-          <!-- card images - end -->
+          <!-- render images - end -->
 
-          <!-- card description -->
+          <!-- renders title, description and date -->
           <b-card-body class="app__components__events__card__body">
             <b-card-text class="app__components__events__card__body__text">
               <h2>
                 <b>{{ event.title }}</b>
               </h2>
-              <p>14/05 at 22:00 - 22/05 at 03:00</p>
+              <p>
+                {{ new Date(event.start).toUTCString().slice(0, 17) }} -
+                {{ new Date(event.end).toUTCString().slice(0, 17) }}
+              </p>
               <i>{{ event.description }}</i>
             </b-card-text>
           </b-card-body>
-          <!-- card description - end -->
+          <!-- renders title, description and date - end -->
 
           <hr />
 
-          <!-- card list of recurrence, category and costs -->
+          <!-- list of recurrence, category and costs of events -->
           <b-row class="text-center app__components__events__card__list">
             <b-col>
               Recurrence:
@@ -74,7 +104,7 @@
               <span>{{ event.costs }}</span>
             </b-col>
           </b-row>
-          <!-- card list of recurrence, category and costs - end -->
+          <!--list of recurrence, category and costs of events - end -->
 
           <!--  card footer with venue -->
           <template v-slot:footer>
@@ -90,8 +120,8 @@
           <!--  card footer with venue - end -->
         </b-card>
         <!-- bootstrap data card - end -->
-      </div>
-    </section>
+      </section>
+    </div>
   </div>
 </template>
 
--- src/events.js
+++ src/events.js
@@ -1,14 +1,19 @@
 export default {
   data() {
     return {
+      //this is a datepicker v-model value
+      filterDate: null,
+      //this disables dates prior to today's date using the min-date of the datepicker
+      min: new Date(),
+      //this is a json object of events
       events: [
         {
           title: "Rock im Park",
           description:
             "Rock im Park is a music festival held annually in May or June at the Volkspark Dutzendteich in Nuremberg and a parallel event to Rock am Ring with an almost identical line-up.",
           image: "rock",
-          start: 1559890800,
-          end: 1560114000,
+          start: 1591833600000,
+          end: 1592006400000,
           recurrence: "yearly",
           costs: "239,00€",
           link: "https://www.rock-im-park.com/",
@@ -25,8 +30,8 @@ export default {
           description:
             "The Oktoberfest in Munich is the biggest folk festival in the world. It takes place since 1810 on the Theresienwiese in the Bavarian capital Munich.",
           image: "oktoberfest",
-          start: 1569016800,
-          end: 1570399199,
+          start: 1600387200000,
+          end: 1601683200000,
           recurrence: "monthly",
           costs: "0,00€",
           link: "https://www.oktoberfest.de/",
@@ -43,8 +48,8 @@ export default {
           description:
             "Half of all life in the universe has been wiped out and there seems to be only one possible future. Do the Avengers and their allied superheroes have what they need to lift the crushing power of the Infinity Stones?",
           image: "avengers",
-          start: 1556143140,
-          end: 1556154000,
+          start: 1593561600000,
+          end: 1596153600000,
           recurrence: "daily",
           costs: "16,00€",
           link: "https://www.cinecitta.de/",
@@ -61,8 +66,8 @@ export default {
           description:
             "The PCIM Europe is the world's leading exhibition and conference for power electronics, intelligent motion, renewable energy, and energy management.",
           image: "pcim",
-          start: 1557230400,
-          end: 1557403200,
+          start: 1594080000000,
+          end: 1594166400000,
           recurrence: "none",
           costs: "24,00€",
           link: "https://pcim.mesago.com/events/en.html",
@@ -76,5 +81,21 @@ export default {
         }
       ]
     };
+  },
+  computed: {
+    //this is a filter that returns events between their respective start and end dates through the boostrap datepicker
+    filteredData() {
+      var date = this.filterDate;
+      return this.events.filter(event => {
+        if (date === null) {
+          return this.events;
+        } else {
+          return (
+            date >= new Date(event.start).toISOString().slice(0, 10) &&
+            date <= new Date(event.end).toISOString().slice(0, 10)
+          );
+        }
+      });
+    }
   }
 };
